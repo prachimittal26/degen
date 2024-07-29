66d97fb3a33696add82d6e5f078845a7238f0cad
Prachi <109284439+prachimittal26@users.noreply.github.com>
2024-07-29 22:04:41 +0530

    Update degen.sol

--- degen.sol
+++ degen.sol
@@ -6,12 +6,7 @@ contract Degen {
     string public assetSymbol;
     address public assetOwner;
 
-    mapping(address => bool) private hasItem1;
-    mapping(address => bool) private hasItem2;
-    mapping(address => bool) private hasItem3;
-    mapping(address => bool) private hasItem4;
     mapping(address => uint256[4]) private redeemedItems;
-
     mapping(address => uint256) private balances;
 
     constructor() {
@@ -47,31 +42,29 @@ contract Degen {
     }
     
     function showRedeemableItems() public pure returns (string memory) {
-        return "1. Item1(100 AVAX)  2. Item2(100 AVAX)  3. Item3(30 AVAX)  4. Item4(50 AVAX)";
+        return "1. Item1(100 GA)  2. Item2(100 GA)  3. Item3(30 GA)  4. Item4(50 GA)";
     }
 
     function redeemAsset(uint256 item, uint256 quantity) public {
         require(quantity > 0, "Quantity must be greater than zero");
 
         if (item == 1) {
-            redeemItem(quantity, 100, hasItem1, 0);
+            redeemItem(quantity, 100, 0);
         } else if (item == 2) {
-            redeemItem(quantity, 100, hasItem2, 1);
+            redeemItem(quantity, 100, 1);
         } else if (item == 3) {
-            redeemItem(quantity, 30, hasItem3, 2);
+            redeemItem(quantity, 30, 2);
         } else if (item == 4) {
-            redeemItem(quantity, 50, hasItem4, 3);
+            redeemItem(quantity, 50, 3);
         } else {
             revert("Invalid item");
         }
     }
 
-    function redeemItem(uint256 quantity, uint256 price, mapping(address => bool) storage itemRedemptionStatus, uint256 itemIndex) private {
+    function redeemItem(uint256 quantity, uint256 price, uint256 itemIndex) private {
         uint256 requiredBalance = price * quantity;
         require(balances[msg.sender] >= requiredBalance, "Insufficient balance for item redemption");
-        require(!itemRedemptionStatus[msg.sender], "Item already redeemed");
 
-        itemRedemptionStatus[msg.sender] = true;
         balances[msg.sender] -= requiredBalance;
         redeemedItems[msg.sender][itemIndex] += quantity;
     }
